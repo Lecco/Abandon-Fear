8ecbb194384f2cd1cf4f791c4bd511dcf596ed8a
O.Pulkrt <lecco@archlinux.localdomain>
2012-04-21 15:42:16 +0200

    Added function for moving of character

--- js/game-core.js
+++ js/game-core.js
@@ -1,11 +1,32 @@
+/* Global variables */
+var playgroundSizeX = 8;
+var playgroundSizeY = 8;
+var fieldSize = 20;       // size of playfield in pixels
+
 /**
  * Constructor for characters (hero, NPC)
  */
 var Character = function(x, y, picture)
 {
-  this.coordinatesX = x;
-  this.coordinatesY = y;
+  this.coordinateX = x;
+  this.coordinateY = y;
   this.picture = picture;
+  this.moveLeft = function(){
+      if (this.coordinateX > 1)
+        this.coordinateX--;
+    }
+  this.moveUp = function(){
+      if (this.coordinateY > 1)
+        this.coordinateY--;
+    }
+  this.moveRight = function(){
+      if (this.coordinateX < playgroundSizeX)
+        this.coordinateX++;
+    }
+  this.moveDown = function(){
+      if (this.coordinateY < playgroundSizeY)
+        this.coordinateY++;
+    }
 }
 /**
  * Constructor for any obsatcles in the way like tables, barrels
@@ -20,9 +41,9 @@ var Barrier = function(x, y, sizeX, sizeY, picture, movable)
 }
 
 /* game elements */
-var hero = new Character(1, 2, 'images/hero.png');
-var enemy = new Character(5, 7, 'images/zombie1.png');
-var table = new Barrier(3, 3, 1, 2, 'images/table.png', true);
+var hero = new Character(1, 2, "images/hero.png");
+var enemy = new Character(5, 7, "images/zombie1.png");
+var table = new Barrier(3, 3, 1, 2, "images/table.png", true);
 
 
 
@@ -34,22 +55,23 @@ function handleKey(e)
 {
   switch (e.keyCode)
   {
-    case '37':
+    case 37:
       hero.moveLeft();
       break;
-    case '38':
+    case 38:
       hero.moveUp();
       break;
-    case '39':
+    case 39:
       hero.moveRight();
       break;
-    case '40':
+    case 40:
       hero.moveDown();
       break;
     default:
-      window.alert('Co to? ' + e.keyCode);
+      window.alert("Co to? " + e.keyCode);
       break;
   }
+  window.alert("Hrdina: x = " + hero.coordinateX + "; y = " + hero.coordinateY);
 }
 document.onkeydown = handleKey;
 
