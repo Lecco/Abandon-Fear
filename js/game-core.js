8adae7cf3bf27dcbd8786982a89eb9aad135d0e4
O.Pulkrt <lecco@archlinux.localdomain>
2012-04-21 15:17:12 +0200

    Added constructors for characters (hero and npc) and barriers like tables

--- js/game-core.js
+++ js/game-core.js
@@ -1,22 +1,55 @@
-(function ($)
+/**
+ * Constructor for characters (hero, NPC)
+ */
+var Character = function(x, y, picture)
 {
-  $.fn.extend(
+  this.coordinatesX = x;
+  this.coordinatesY = y;
+  this.picture = picture;
+}
+/**
+ * Constructor for any obsatcles in the way like tables, barrels
+ */
+var Barrier = function(x, y, sizeX, sizeY, picture, movable)
+{
+  this.coordinateX = x;
+  this.coordinateY = y;
+  this.sizeX = sizeX;
+  this.sizeY = sizeY;
+  this.movable = movable;
+}
+
+/* game elements */
+var hero = new Character(1, 2, 'images/hero.png');
+var enemy = new Character(5, 7, 'images/zombie1.png');
+var table = new Barrier(3, 3, 1, 2, 'images/table.png', true);
+
+
+
+
+/**
+ * Function to handle pressed keys
+ */
+function handleKey(e)
+{
+  switch (e.keyCode)
   {
-    zombie-game: function(options)
-    {
-      var defaults = {
-        sizeX : 20,
-        sizeY : 20
-      }
+    case '37':
+      hero.moveLeft();
+      break;
+    case '38':
+      hero.moveUp();
+      break;
+    case '39':
+      hero.moveRight();
+      break;
+    case '40':
+      hero.moveDown();
+      break;
+    default:
+      window.alert('Co to? ' + e.keyCode);
+      break;
+  }
+}
+document.onkeydown = handleKey;
 
-      var options = $.extend(defaults, options);
-
-      return this.each(function()
-      {
-        var o = options;
-
-        // alert(o.sizeX);
-      });
-    }
-  });
-})(jQuery);
