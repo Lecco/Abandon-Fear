477528de3d76a9b4e95c71c1537458ed5c9fd08e
O.Pulkrt <lecco@archlinux.localdomain>
2012-04-21 11:43:44 +0200

    Updated game-core = jQuery plugin now It would be hard to finish with global variables so I decided to make it as jQuery plugin with its own functions

--- js/game-core.js
+++ js/game-core.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-  
-});
+(function ($)
+{
+  $.fn.extend(
+  {
+    zombie-game: function(options)
+    {
+      var defaults = {
+        sizeX : 20,
+        sizeY : 20
+      }
+
+      var options = $.extend(defaults, options);
+
+      return this.each(function()
+      {
+        var o = options;
+
+        // alert(o.sizeX);
+      });
+    }
+  });
+})(jQuery);
