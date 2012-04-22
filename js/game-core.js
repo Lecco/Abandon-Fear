b35d1b878ffafd017a7d8be28bc24d2edcb44204
O.Pulkrt <lecco@archlinux.localdomain>
2012-04-22 19:19:14 +0200

    Added default styles of object in game(position:absolute) and movement Added function for moving object to their initial places and ther same for moving in any direction (up, down, left right)

--- js/game-core.js
+++ js/game-core.js
@@ -1,58 +1,93 @@
-/* Global variables */
-var playgroundSizeX = 8;
-var playgroundSizeY = 8;
-var fieldSize = 20;       // size of playfield in pixels
+/* Game variables */
+var playground,
+    hero,
+    enemy,
+    table;
+
+/**
+ * Function to add content to game screen
+ */
+function addToPlayground(content)
+{
+  document.getElementById("playground").innerHTML = document.getElementById("playground").innerHTML + content;
+}
+
+/**
+ * Constructor for playground variables and functions
+ */
+var Playground = function(sizeX, sizeY, fieldSize)
+{
+  this.sizeX = sizeX;
+  this.sizeY = sizeY;
+  this.fieldSize = fieldSize;
+
+  document.body.innerHTML = "<div id='playground'></div>";
+  document.getElementById("playground").style.width = (this.sizeX * fieldSize) + "px";
+  document.getElementById("playground").style.height = (this.sizeY * fieldSize) + "px";
+}
 
 /**
  * Constructor for characters (hero, NPC)
  */
-var Character = function(x, y, picture)
+var Character = function(name, x, y, picture)
 {
+  this.name = name;
   this.coordinateX = x;
   this.coordinateY = y;
   this.picture = picture;
+
   this.moveLeft = function(){
       if (this.coordinateX > 1)
         this.coordinateX--;
+      this.move(this.coordinateX, this.coordinateY);
     }
   this.moveUp = function(){
       if (this.coordinateY > 1)
         this.coordinateY--;
+      this.move(this.coordinateX, this.coordinateY);
     }
   this.moveRight = function(){
-      if (this.coordinateX < playgroundSizeX)
+      if (this.coordinateX < playground.sizeX)
         this.coordinateX++;
+      this.move(this.coordinateX, this.coordinateY);
     }
   this.moveDown = function(){
-      if (this.coordinateY < playgroundSizeY)
+      if (this.coordinateY < playground.sizeY)
         this.coordinateY++;
+      this.move(this.coordinateX, this.coordinateY);
     }
+  this.move = function(x, y){
+      document.getElementById("character_" + this.name).style.left = ((this.coordinateX - 1) * playground.fieldSize) + "px";
+      document.getElementById("character_" + this.name).style.top = ((this.coordinateY - 1) * playground.fieldSize) + "px";
+    }
+  addToPlayground("<div id='character_" + this.name + "'><img src='" + this.picture + "' style='width:" + playground.fieldSize + "px;height:" + playground.fieldSize + "px'></div>");
+  this.move(this.coordinateX, this.coordinateY);
 }
+
 /**
  * Constructor for any obsatcles in the way like tables, barrels
  */
-var Barrier = function(x, y, sizeX, sizeY, picture, movable)
+var Barrier = function(name, x, y, sizeX, sizeY, picture, movable)
 {
+  this.name = name;
   this.coordinateX = x;
   this.coordinateY = y;
   this.sizeX = sizeX;
   this.sizeY = sizeY;
+  this.picture = picture;
   this.movable = movable;
+
+  addToPlayground("<div id='barrier_" + this.name + "'><img src='" + this.picture + "' style='width:" + playground.fieldSize + "px;height:" + playground.fieldSize + "px'></div>");
+  document.getElementById("barrier_" + this.name).style.left = ((this.coordinateX - 1) * playground.fieldSize) + "px";
+  document.getElementById("barrier_" + this.name).style.top = ((this.coordinateY - 1) * playground.fieldSize) + "px";
 }
-
-/* game elements */
-var hero = new Character(1, 2, "images/hero.png");
-var enemy = new Character(5, 7, "images/zombie1.png");
-var table = new Barrier(3, 3, 1, 2, "images/table.png", true);
-
-
-
 
 /**
  * Function to handle pressed keys
  */
 function handleKey(e)
 {
+  //window.alert("Hrdina " + hero.name + ": x = " + hero.coordinateX + "; y = " + hero.coordinateY);
   switch (e.keyCode)
   {
     case 37:
@@ -68,10 +103,24 @@ function handleKey(e)
       hero.moveDown();
       break;
     default:
-      window.alert("Co to? " + e.keyCode);
+      //window.alert("Co to? " + e.keyCode);
       break;
   }
-  window.alert("Hrdina: x = " + hero.coordinateX + "; y = " + hero.coordinateY);
 }
-document.onkeydown = handleKey;
 
+/**
+ * Initialize game variables and start the game
+ */
+function initGame()
+{
+  document.onkeydown = handleKey;
+  /* game elements */
+  playground = new Playground(8, 8, 30);
+  hero = new Character("main", 1, 2, "images/hero.jpg");
+  enemy = new Character("zombie", 5, 7, "images/zombie.gif");
+  table = new Barrier("table", 3, 3, 1, 1, "images/table.gif", true);
+
+  
+}
+
+window.onload = initGame;
